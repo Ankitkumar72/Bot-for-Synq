3f59ab8b014691f601587ec741aef3d10ff2c2f3
Ankit Kumar <14420068m@gmail.com>
2026-02-17 20:53:26 +0530

    Refactor and add new functionality for palindrome and second largest number checks

--- MATH/commonfactor2427.py
+++ MATH/commonfactor2427.py
@@ -1,13 +1,10 @@
-import math
-
-a, b = map(int, input().split())
-
-g = math.gcd(a, b)
-count = 0
-for i in range(1, int(g ** 0.5) + 1):
-    if g % i == 0:
-        count += 1
-        if i != g // i:
-            count += 1
-
-print(count)
+n = int (input())
+num = n
+nod = len(str(n))
+total = 0
+while num > 0:
+    ld = num %10 
+    total = total + (ld ** nod)
+    num = num // 10
+print(total == n)
+    
