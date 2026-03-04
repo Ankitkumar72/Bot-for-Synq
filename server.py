68eab6aa312dbcadb637a158594fddacd423f325
Ankit Kumar <14420068m@gmail.com>
2026-03-04 13:56:06 +0530

    update code with fixed names

--- server.py
+++ server.py
@@ -17,26 +17,22 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_member_join(member):
     
-    channel = discord.utils.get(member.guild.channels, name="👋 | introductions")
+    channel = discord.utils.get(member.guild.channels, name="👋-introductions")
     
     if channel:
         welcome_1_liner = f"Welcome to the community, {member.mention}! Glad to have you here. "
         
-        template = (
-            "**Please introduce yourself so we can get to know you:**\n"
-            "```\n"
-            "Name: \n"
-            "Role: \n"
-            "What I'm building: \n"
-            "```"
-        )
-        
-        await channel.send(f"{welcome_1_liner}\n\n{template}")
+        await channel.send(welcome_1_liner)
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"| Pong! {round(bot.latency * 500)}ms")
+
+@bot.command()
+async def testjoin(ctx):
+    
+    await on_member_join(ctx.author)
 
 
 if TOKEN:
