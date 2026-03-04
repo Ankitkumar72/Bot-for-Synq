5682dd94c7e4a9e70cf2a3030dc90519f36f8dce
Ankit Kumar <14420068m@gmail.com>
2026-03-04 14:17:15 +0530

    Porting updates for render

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,7 +9,9 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render assigns a dynamically generated port. We need to fetch it dynamically.
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
